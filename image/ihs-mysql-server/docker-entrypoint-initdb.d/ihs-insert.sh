ed1f5c2d0e800058eb7352d390f654c530f8d6b7
Greg Kostin <gkostin@umich.edu>
2022-04-12 09:52:50 -0400

    image refactoring

--- image/ihs-mysql-server/docker-entrypoint-initdb.d/ihs-insert.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sed "s/PREFIX/${INDEPENDENT_HANDLE_SERVER_PREFIX}/g" /ihs/ihs-insert.sql | \
-sed "s/PASSWORD/${MARIADB_PASSWORD}/g" | \
-mysql --user $MARIADB_USER --password=$MARIADB_PASSWORD $MARIADB_DATABASE
--- image/ihs-mysql-server/docker-entrypoint-initdb.d/ihs-select.sql
+++
@@ -1,3 +0,0 @@
-SELECT * FROM nas;
-
-SELECT * FROM handles;
