a28ef5d081d69489e8204906c45f4add4cec4ccd
Greg Kostin <gkostin@umich.edu>
2022-04-13 10:50:20 -0400

    dotenv

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 [![Ruby Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://github.com/testdouble/standard)
 ![CI](https://github.com/mlibrary/handle_rest/actions/workflows/ci.yml/badge.svg)
 
-# Handle 
+# Handle Rest
 
 Ruby interface to the [CNRI Handle System](http://handle.net/) REST API.  The
 CNRI Handle Server provides a DNS-like method of providing persistent URLs to
--- bin/yml-envsubt.sh
+++ bin/yml-envsubt.sh
@@ -2,8 +2,11 @@
 
 pwd=$(pwd)
 echo "pwd: $pwd"
+
 echo "script: $0"
 echo "script dirname: $(dirname $0)"
+bin=$(dirname $0)
+
 echo "file: $1"
 echo "file dirname: $(dirname $1)"
 
@@ -14,27 +17,27 @@ echo "file_name: $file_name"
 file_extension="${file_name##*.}"
 echo "file_extension: $file_extension"
 file_base="${file_name%.*}"
-echo "file_base: $file"
-
-export YML_MY_APP="testbed"
-echo "app: $YML_MY_APP"
+echo "file_base: $file_base"
 export YML_MY_NAME=$file_base
 echo "name: $YML_MY_NAME"
+
 export GITHUB_PACKAGES_READ=`echo "${GITHUB_USER}:${GITHUB_READ_PACKAGES_TOKEN}" | base64`
 echo "github_packages_read: $GITHUB_PACKAGES_READ"
 
-export YML_MY_MARIADB_HOST=mysql
+eval "$($bin/shdotenv)"
+#export YML_MY_APP="testbed"
+echo "app: $YML_MY_APP"
+#export YML_MY_MARIADB_HOST=mysql
 echo "mariadb host: $YML_MY_MARIADB_HOST"
-export YML_MY_MARIADB_ROOT_PASSWORD=password
+#export YML_MY_MARIADB_ROOT_PASSWORD=password
 echo "mariadb root password: $YML_MY_MARIADB_ROOT_PASSWORD"
-export YML_MY_MARIADB_USER=ihs
+#export YML_MY_MARIADB_USER=ihs
 echo "mariadb user: $YML_MY_MARIADB_USER"
-export YML_MY_MARIADB_PASSWORD=password
+#export YML_MY_MARIADB_PASSWORD=password
 echo "mariadb password: $YML_MY_MARIADB_PASSWORD"
-export YML_MY_MARIADB_DATABASE=ihs
+#export YML_MY_MARIADB_DATABASE=ihs
 echo "mariadb database: $YML_MY_MARIADB_DATABASE"
-
-export YML_MY_INDEPENDENT_HANDLE_SERVER_PREFIX=IHS
+#export YML_MY_INDEPENDENT_HANDLE_SERVER_PREFIX=IHS
 echo "IHS prefex: $YML_MY_INDEPENDENT_HANDLE_SERVER_PREFIX"
 
-envsubst < $1.envsubst > $1
+#envsubst < $1.envsubst > $1
